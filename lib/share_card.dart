1b60d5e327ba27de9b6c96aade90085424b28eeb
Jordan Holland <jordan.mark.holland@gmail.com>
2021-07-21 13:14:20 +1000

    Create ShareCard widget.

--- lib/share_card.dart
+++ lib/share_card.dart
@@ -1,7 +1,68 @@
 library share_card;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class InfoBarStyle {
+  const InfoBarStyle({
+    this.padding = const EdgeInsets.all(20),
+    this.backgroundColor = Colors.grey,
+    this.iconWidget,
+  });
+  final EdgeInsets padding;
+  final Color backgroundColor;
+  final Widget? iconWidget;
+}
+
+class ShareCard extends StatelessWidget {
+  const ShareCard({
+    Key? key,
+    required this.child,
+    required this.title,
+    required this.subtitle,
+    this.margin = const EdgeInsets.all(0),
+    this.infoBarStyle = const InfoBarStyle(),
+    this.borderRadius,
+  }) : super(key: key);
+  final Widget child;
+  final Widget title;
+  final Widget subtitle;
+  final EdgeInsets margin;
+  final InfoBarStyle infoBarStyle;
+  final BorderRadius? borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: margin,
+      decoration: BoxDecoration(
+        borderRadius: borderRadius ?? BorderRadius.circular(20),
+      ),
+      child: ClipRRect(
+        borderRadius: borderRadius ?? BorderRadius.circular(20),
+        child: Column(
+          children: [
+            Container(
+              child: child,
+            ),
+            Container(
+              padding: infoBarStyle.padding,
+              width: double.infinity,
+              color: infoBarStyle.backgroundColor,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  if (infoBarStyle.iconWidget != null) infoBarStyle.iconWidget!,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [title, subtitle],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
