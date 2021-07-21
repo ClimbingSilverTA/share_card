62eace3e88486c73f3decb02a66ca767c21f95b4
Jordan Holland <jordan.mark.holland@gmail.com>
2021-07-22 08:52:55 +1000

    Add optional pixelRatio argument.

--- lib/src/share_card_controller.dart
+++ lib/src/share_card_controller.dart
@@ -8,10 +8,14 @@ class ShareCardController extends ChangeNotifier {
   ShareCardController({GlobalKey? key}) : key = key ?? GlobalKey();
   final GlobalKey key;
 
-  Future<Uint8List?> getImageBytes() async {
+  Future<Uint8List?> getImageBytes({
+    double pixelRatio = 1,
+  }) async {
     final boundary =
         key.currentContext?.findRenderObject() as RenderRepaintBoundary?;
-    final image = await boundary?.toImage();
+    final image = await boundary?.toImage(
+      pixelRatio: pixelRatio,
+    );
     final byteData = await image?.toByteData(format: ImageByteFormat.png);
     final imageBytes = byteData?.buffer.asUint8List();
     return imageBytes;
